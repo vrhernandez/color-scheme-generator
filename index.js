5355722d89b6380508b01970ca238fe18ccffd8d
vrhernandez <vicenterh99@gmail.com>
2024-12-03 22:36:26 -0800

    connected to Color API, and render DOM elements with response data

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const schemeForm = document.getElementById("scheme-form")
 
+schemeForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    
+    const baseURL = "https://www.thecolorapi.com"
+    const hex = document.getElementById("color-picker").value.substring(1)
+    const mode = document.getElementById("mode-selector").value
 
-schemeForm.addEventListener("submit", () => {
-const baseURL = "https://www.thecolorapi.com"
-const hex = "000000"
-const mode = "monochrome"
-
-fetch(`${baseURL}/scheme?hex=${hex}&mode=${mode}`)
-    .then(res => res.json())
-    .then(data => {
-        const colorList = data.colors
-        colorList.forEach(color => {
-            console.log(color.hex.value)
-        });
-    })
+    fetch(`${baseURL}/scheme?hex=${hex}&mode=${mode}`)
+        .then(res => res.json())
+        .then(data => {
+            const colorList = data.colors
+            colorList.forEach(function(color, index) {
+                const colorBarEl = document.getElementById(`color-${index}`)
+                const hexEl = document.getElementById(`hex-${index}`)
+                colorBarEl.style.backgroundColor = color.hex.value
+                hexEl.textContent = color.hex.value
+            })
+        })
 })
